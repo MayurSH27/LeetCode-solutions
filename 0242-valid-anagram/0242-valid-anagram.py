ad72ef7dbf9343dd7c70b2322cc87a5928a3fcbc
Mayur Hanagandi <153411831+MayurSH27@users.noreply.github.com>
2026-08-09 11:13:27 +0530

    Time: 19 ms (16.49%), Space: 20.2 MB (18.6%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,26 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        hm1 = {}
-        hm2 = {}
-
-        for i in s:
-            if i in hm1:
-                hm1[i] = hm1[i]+1
-            else:
-                hm1[i]=1
-        
-        for i in t:
-            if i in hm2:
-                hm2[i] = hm2[i]+1
-            else:
-                hm2[i]=1
-
-        if(len(hm1)!=len(hm2)):
+        if len(s)!=len(t):
             return False
 
-        for i in hm1:
-            if i not in hm2:
-                return False
-            elif hm1[i]!=hm2[i]:
+        s1 = list(s)
+        t1 = list(t)
+
+        s1.sort()
+        t1.sort()
+
+        for i in range(len(s)):
+            if(s1[i]!=t1[i]):
                 return False
         return True
