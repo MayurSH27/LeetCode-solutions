39f11e60f4bdcfb069f1d914be6c965dbdabff9d
Mayur Hanagandi <153411831+MayurSH27@users.noreply.github.com>
2026-08-09 11:10:24 +0530

    Time: 7 ms (91.61%), Space: 19.5 MB (46.46%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,26 @@
-from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        hm1 = {}
+        hm2 = {}
+
+        for i in s:
+            if i in hm1:
+                hm1[i] = hm1[i]+1
+            else:
+                hm1[i]=1
+        
+        for i in t:
+            if i in hm2:
+                hm2[i] = hm2[i]+1
+            else:
+                hm2[i]=1
+
+        if(len(hm1)!=len(hm2)):
+            return False
+
+        for i in hm1:
+            if i not in hm2:
+                return False
+            elif hm1[i]!=hm2[i]:
+                return False
+        return True
