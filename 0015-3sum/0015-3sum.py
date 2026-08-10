a1390da801920bb8949900cb9ccdafaaedbb8c68
Mayur Hanagandi <153411831+MayurSH27@users.noreply.github.com>
2026-08-10 14:34:12 +0530

    Time: 507 ms (85.45%), Space: 22.4 MB (31.76%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,29 +4,40 @@ class Solution:
         ans = []
         n = len(nums)
 
-        for i in range(n-2):
-            if i>0 and nums[i] == nums[i-1]:
+        for i in range(n - 2):
+
+            # Skip duplicate first elements
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            j=i+1
-            k=n-1
 
-            while j<k:
-                s = nums[i]+nums[j]+nums[k]
-                if s==0:
+            # Since array is sorted, this is impossible
+            if nums[i] > 0:
+                break
+
+            j = i + 1
+            k = n - 1
+
+            while j < k:
+                s = nums[i] + nums[j] + nums[k]
+
+                if s == 0:
                     ans.append([nums[i], nums[j], nums[k]])
-                    j+=1
-                    k-=1
 
-                    while j<k and nums[j] == nums[j-1]:
-                        j+=1
+                    j += 1
+                    k -= 1
 
-                    while j<k and nums[k] == nums[k+1]:
-                        k-=1
-                
-                elif s<0:
-                    j+=1
+                    # Skip duplicate j values
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+
+                    # Skip duplicate k values
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
+
+                elif s < 0:
+                    j += 1
 
                 else:
-                    k-=1
+                    k -= 1
 
         return ans
